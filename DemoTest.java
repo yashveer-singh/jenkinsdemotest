c8322da20bc1045dc45836fee48516f74b75b900
yashveer-singh <yashveersingh10528@gmail.com>
2024-04-15 14:36:28 +0530

    add intial

---
+++ DemoTest.java
@@ -0,0 +1,15 @@
+
+public class DemoTest {
+
+   public  static void fun(){
+
+        System.out.println("hello yashveer");
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Start jenkins pipeline.......");
+
+        fun();
+    } 
+    }
